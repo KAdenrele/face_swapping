bcbdc80f20668a9c3af475dc98eb8608798f2e14
KAdenrele <akomolafe@zaizi.com>
2026-01-22 11:27:20 +0000

    output directory corrected

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@
 
 # --- File Paths and Directories ---
 INPUT_IMAGE_DIR = "data/input_images"
-OUTPUT_DIR = "data/inswapper_set"
+OUTPUT_DIR = "data/inswapper_images"
 MODEL_DIR = "models"
 SWAPPER_MODEL_PATH = f"{MODEL_DIR}/inswapper_128.onnx"
 
@@ -11,7 +11,6 @@ FACE_ANALYZER_MODEL = "buffalo_l"
 
 # --- Dataset Curation Parameters ---
 IMAGES_PER_CATEGORY = 800 # This will generate 4 * 800 = 3200 total images
-
 # Parameters for selecting similar/dissimilar pairs
 SIMILAR_MAX_AGE_DIFF = 10
 DISSIMILAR_MIN_AGE_DIFF = 15
