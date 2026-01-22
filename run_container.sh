6e5eee509cf499410aaddb5dbbe12c7f07c3e93f
KAdenrele <akomolafe@zaizi.com>
2026-01-22 13:36:32 +0000

    container command

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,1 @@
-docker build -t face_swapper_app . && docker run --name inswapper-container -v /mnt/data2/test_dataset/raw/:/data face_swapper_app
+docker build -t face_swapper_app . && docker run --name inswapper-container -v /mnt/data/test_dataset/raw/:/data face_swapper_app
